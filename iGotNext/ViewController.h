a66b7bfa50c69da0b7bba0385fd8e473d7c0e8a4
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-02-09 10:20:16 -0600

    Initial Commit

---
+++ iGotNext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iGotNext
+//
+//  Created by Kyle Magnesen on 2/9/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
