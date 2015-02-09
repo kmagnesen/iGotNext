f65ade5f801019f530de104d3d5ead9de36feb4c
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-02-09 11:11:37 -0600

    Initial Commit

--- iGotNext/HomeFeedViewController.h
+++ iGotNext/HomeFeedViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeFeedViewController.h
 //  iGotNext
 //
 //  Created by Kyle Magnesen on 2/9/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HomeFeedViewController : UIViewController
 
 @end
-
