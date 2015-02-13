a4f61038d9427bb0c00866c7746f18541687a57a
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-02-13 14:16:07 -0600

    Pinned current location

--- iGotNext/HomeParksViewController.m
+++ iGotNext/HomeParksViewController.m
@@ -21,6 +21,7 @@
 @property (strong, nonatomic) IBOutlet UISegmentedControl *segmentController;
 @property NSArray *mapItems;
 @property MKMapItem *selectedPark;
+@property MKPointAnnotation *parksAnnotation;
 
 @end
 
@@ -36,6 +37,13 @@
     [self.locationManager startUpdatingLocation];
 
     self.selectedPark = [MKMapItem new];
+    self.parksAnnotation = [MKPointAnnotation new];
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    self.mapView.showsUserLocation = YES;
+
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
@@ -103,6 +111,38 @@
         [self.mapView setHidden:NO];
     }
 }
+
+#pragma mark - MK Map View Methods
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+
+
+    if (annotation == mapView.userLocation) {
+        return nil;
+    }
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+
+    pin.canShowCallout = YES;
+//    else if (annotation != mapView.userLocation) {
+//    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+//    }
+    return pin;
+
+}
+
+//- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+//    CLLocationCoordinate2D centerCoordinate = view.annotation.coordinate;
+//
+//    MKCoordinateSpan span;
+//    span.latitudeDelta = 0.01;
+//    span.longitudeDelta = 0.01;
+//
+//    MKCoordinateRegion region;
+//    region.center = centerCoordinate;
+//    region.span = span;
+//
+//    [self.mapView setRegion:region animated:YES];
+//}
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     self.selectedPark = [self.mapItems objectAtIndex:self.tableView.indexPathForSelectedRow.row];
