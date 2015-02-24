98052f97848f4d151a6bae638ba9371a3e544c6d
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-02-24 08:42:14 -0600

    Profile Modal view

---
+++ iGotNext/EditPicCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  EditPicCollectionViewCell.m
+//  iGotNext
+//
+//  Created by Kyle Magnesen on 2/24/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "EditPicCollectionViewCell.h"
+
+@implementation EditPicCollectionViewCell
+
+@end
